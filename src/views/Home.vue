80426104581d2fd2a2d0f9489548f48924101e33
wanghongke <wanghongke@infohold.com.cn>
2021-01-14 16:36:49 +0800

    init base code files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <img alt="Vue logo" :src="girlImg.imgUrl" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -8,11 +9,24 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { reactive } from 'vue'
+
+interface Image {
+  imgUrl: string;
+}
 
 @Options({
   components: {
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  girlImg = reactive<Image>({ imgUrl: '' })
+  mounted () {
+    this.axios.get('https://apiblog.jspang.com/default/getGirl').then(res => {
+      this.girlImg = res.data
+      console.log(this.girlImg)
+    })
+  }
+}
 </script>
